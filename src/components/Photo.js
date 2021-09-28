5f6b675cfd96a0a112b552ad045507baac180c5e
david-mcgrouther <dbmcgrouther@gmail.com>
2021-09-27 17:49:55 -0700

    the text in the textarea tag will create a link when submitted

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -5,7 +5,20 @@ function Photo() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    window.open("https://assets.imgix.net/examples/butterfly.jpg");
+
+    // console.log(text);
+    let textToArray=text.split(' ');
+    let textForLink=textToArray[0];
+    
+    for(let i = 1; i < textToArray.length; i++){
+      console.log(`textToArray is ${textToArray}`);
+      textForLink = textForLink + '%20' + textToArray[i];
+    }
+
+    // let fullUrl = "https://assets.imgix.net/examples/butterfly.jpg?txt=Check%20out%20my%20sweet%20wings&blend=4400bb&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50"
+    let baseUrl = "https://assets.imgix.net/examples/butterfly.jpg?txt=";
+    let endPartOfURL = "&blend=4400bb&w=640&txtclr=fff&txtalign=center%2Cmiddle&txtsize=48&bm=normal&balph=50";
+    window.open(baseUrl + textForLink + endPartOfURL);
   }
 
   return(
